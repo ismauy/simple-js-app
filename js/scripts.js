d14d7c9f4f524b48337d87ed3f96b2094b45cf50
ismauy <isma.ferrazzi@gmail.com>
2021-07-29 10:35:39 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+let pokemonList = [
+  {
+  	name: 'Jigglypuff',
+  	height: 50,
+  	types: ['fairy', 'normal']
+  },
+
+  {
+  	name: 'Paras',
+  	height: 30,
+  	types: ['grass', 'bug']
+  },
+
+  {
+  	name: 'Ledian',
+  	height: 140,
+  	types: ['bug', 'flying']
+  },
+
+  {
+  	name: 'Natu',
+  	height: 20,
+  	types: ['Psychic', 'flying']
+  },
+
+  {
+  	name: 'Numel',
+  	height: 70,
+  	types: ['fire', 'ground']
+  },
+]
