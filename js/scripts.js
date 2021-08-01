27af85a9949bfe11d8b58caf309281b6a0ae3f23
ismauy <isma.ferrazzi@gmail.com>
2021-08-01 21:19:41 +0200

    loop added

--- js/scripts.js
+++ js/scripts.js
@@ -30,3 +30,11 @@ let pokemonList = [
   	types: ['fire', 'ground']
   },
 ]
+
+for (let i = 0; i < pokemonList.length; i++){
+	if (pokemonList[i].height > 80) {
+		document.write(`<p> ${pokemonList[i].name} (height: ${pokemonList[i].height}") - I am the tallest pokemon! </p>`);
+	} else {
+		document.write(`<p> ${pokemonList[i].name} (height: ${pokemonList[i].height}")</p>`);
+	}
+}
