d2fa8f555bf6bf546bbb511b11cb96d0419216bc
ismauy <isma.ferrazzi@gmail.com>
2021-08-05 22:21:28 +0200

    forEach and IIFE update

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,61 @@
+let pokemonRepository = (function(){
 
-let pokemonList = [
-  {
-  	name: 'Jigglypuff',
-  	height: 50,
-  	types: ['fairy', 'normal']
-  },
+  let pokemonList = [
+    {
+    	name: 'Jigglypuff',
+    	height: 50,
+    	types: ['fairy', 'normal']
+    },
 
-  {
-  	name: 'Paras',
-  	height: 30,
-  	types: ['grass', 'bug']
-  },
+    {
+    	name: 'Paras',
+    	height: 30,
+    	types: ['grass', 'bug']
+    },
 
-  {
-  	name: 'Ledian',
-  	height: 140,
-  	types: ['bug', 'flying']
-  },
+    {
+    	name: 'Ledian',
+    	height: 140,
+    	types: ['bug', 'flying']
+    },
 
-  {
-  	name: 'Natu',
-  	height: 20,
-  	types: ['psychic', 'flying']
-  },
+    {
+    	name: 'Natu',
+    	height: 20,
+    	types: ['psychic', 'flying']
+    },
 
-  {
-  	name: 'Numel',
-  	height: 70,
-  	types: ['fire', 'ground']
-  },
-]
+    {
+    	name: 'Numel',
+    	height: 70,
+    	types: ['fire', 'ground']
+    },
+  ]
+
+function add(pokemon){
+  pokemonList.push(pokemon);
+	}
+
+function getAll () {
+	return pokemonList;
+	}
+
+return {
+	add: add,
+	getAll: getAll,
+	};
+}) ();
+
 
 document.write('<ul>');
-for (let i = 0; i < pokemonList.length; i++) {
-  let currentPokemon = pokemonList[i];
+pokemonRepository.getAll().forEach(function(pokes) {
 
   document.write(`
-    <li>
-      ${currentPokemon.name} (height: <span>${currentPokemon.height}</span>)
+    <li> ${pokes.name} (height: <span>${pokes.height}</span>)
   `);
-  if (currentPokemon.height > 70) {
+  if (pokes.height > 70) {
     document.write(' - I am the tallest pokemon!');
   }
   document.write('</li>');
-}
+})
 document.write('</ul>');
