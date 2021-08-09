2f00b13d88406a5207e987270df5d398d69b8672
ismauy <isma.ferrazzi@gmail.com>
2021-08-09 19:52:01 +0200

    mainly indentation and spacing fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-let pokemonRepository = (function(){
+let pokemonRepository = (function() {
 
   let pokemonList = [
     {
@@ -32,30 +32,30 @@ let pokemonRepository = (function(){
     },
   ]
 
-function add(pokemon){
-  pokemonList.push(pokemon);
-	}
+      function add(pokemon) {
+        pokemonList.push(pokemon);
+      	}
 
-function getAll () {
-	return pokemonList;
-	}
+      function getAll () {
+      	return pokemonList;
+      	}
 
-return {
-	add: add,
-	getAll: getAll,
-	};
-}) ();
+      return {
+      	add: add,
+      	getAll: getAll,
+      	};
+      }) ();
 
 
-document.write('<ul>');
-pokemonRepository.getAll().forEach(function(pokes) {
+    document.write('<ul>');
+    pokemonRepository.getAll().forEach(function(pokes) {
 
-  document.write(`
-    <li> ${pokes.name} (height: <span>${pokes.height}</span>)
-  `);
-  if (pokes.height > 70) {
-    document.write(' - I am the tallest pokemon!');
-  }
-  document.write('</li>');
-})
-document.write('</ul>');
+      document.write(`
+        <li> ${pokes.name} (height: <span>${pokes.height}</span>)
+        `);
+        if (pokes.height > 70) {
+          document.write(' - I am the tallest pokemon!');
+        }
+        document.write('</li>');
+      })
+      document.write('</ul>');
