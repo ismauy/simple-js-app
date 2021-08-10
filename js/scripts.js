b98e5802c4c34081031ca4ff6d9c44ade60f9a33
ismauy <isma.ferrazzi@gmail.com>
2021-08-10 16:18:56 +0200

    indentation and spacing fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,60 @@
 let pokemonRepository = (function() {
 
-  let pokemonList = [
-    {
-    	name: 'Jigglypuff',
-    	height: 50,
-    	types: ['fairy', 'normal']
-    },
+    let pokemonList = [{
+            name: 'Jigglypuff',
+            height: 50,
+            types: ['fairy', 'normal']
+        },
 
-    {
-    	name: 'Paras',
-    	height: 30,
-    	types: ['grass', 'bug']
-    },
+        {
+            name: 'Paras',
+            height: 30,
+            types: ['grass', 'bug']
+        },
 
-    {
-    	name: 'Ledian',
-    	height: 140,
-    	types: ['bug', 'flying']
-    },
+        {
+            name: 'Ledian',
+            height: 140,
+            types: ['bug', 'flying']
+        },
 
-    {
-    	name: 'Natu',
-    	height: 20,
-    	types: ['psychic', 'flying']
-    },
+        {
+            name: 'Natu',
+            height: 20,
+            types: ['psychic', 'flying']
+        },
 
-    {
-    	name: 'Numel',
-    	height: 70,
-    	types: ['fire', 'ground']
-    },
-  ]
+        {
+            name: 'Numel',
+            height: 70,
+            types: ['fire', 'ground']
+        },
+    ]
 
-      function add(pokemon) {
+    function add(pokemon) {
         pokemonList.push(pokemon);
-      	}
+    }
 
-      function getAll () {
-      	return pokemonList;
-      	}
+    function getAll() {
+        return pokemonList;
+    }
 
-      return {
-      	add: add,
-      	getAll: getAll,
-      	};
-      }) ();
+    return {
+        add: add,
+        getAll: getAll,
+    };
+})();
 
 
-    document.write('<ul>');
-    pokemonRepository.getAll().forEach(function(pokes) {
+document.write('<ul>');
+pokemonRepository.getAll().forEach(function(pokes) {
 
-      document.write(`
+    document.write(`
         <li> ${pokes.name} (height: <span>${pokes.height}</span>)
         `);
-        if (pokes.height > 70) {
-          document.write(' - I am the tallest pokemon!');
-        }
-        document.write('</li>');
-      })
-      document.write('</ul>');
+    if (pokes.height > 70) {
+        document.write(' - I am the tallest pokemon!');
+    }
+    document.write('</li>');
+})
+document.write('</ul>');
